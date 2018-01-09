9818276a52983e23b98c72392ea1479c452f31a4
SayNada <33668198+SayNada@users.noreply.github.com>
2018-01-09 15:49:05 +0000

    Update Language.java

--- src/main/java/com/drtshock/playervaults/translations/Language.java
+++ src/main/java/com/drtshock/playervaults/translations/Language.java
@@ -2,7 +2,8 @@ package com.drtshock.playervaults.translations;
 
 public enum Language {
     ENGLISH("english"),
-
+    BULGARIAN("english"),
+    DUTCH("english");
     private String friendlyName;
 
     Language(String friendlyName) {
